1c34f77f49fd202d3d9f3915f39686ab56330d20
dylan <825880445@qq.com>
2025-02-25 15:38:00 +0800

    字符串
    f、title、upper、lower、strip

---
+++ name_cases.py
@@ -0,0 +1,16 @@
+name = "Eric"
+message = f"Hello {name}, would you like to learn some python today"
+print(message)
+
+fullname = "mei li"
+print(fullname.lower())
+print(fullname.upper())
+print(fullname.title())
+
+famous_person = " \n\t老子 \t\n"
+note = f'{famous_person.strip()}曾说过："老子天下第一"'
+print(note)
+
+
+filename = "python_notes.txt"
+print(filename.removesuffix(".txt"))

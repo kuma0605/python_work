556a1d4379434f4cb0871eb59948497041db6066
dylan <825880445@qq.com>
2025-02-28 00:07:32 +0800

    提交代码

---
+++ 6/6_3/user.py
@@ -0,0 +1,14 @@
+user_0 = {
+    'username': 'efermi',
+    'first': 'enrico',
+    'last': 'fermi',
+    }
+for key, value in user_0.items():
+    print(f"\nKey: {key}")
+    print(f"Value: {value}")
+
+for key in user_0.keys():
+    print(key.title())
+
+for key in user_0:
+    print(key.title())
